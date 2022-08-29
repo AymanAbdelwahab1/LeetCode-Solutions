53c0ec05a35f2e44223c03222eaaa13c5a5b62da
Ayman Abdelwahab <25782681+AymanAbdelwahab1@users.noreply.github.com>
2022-08-29 02:49:43 -0700

    Time: 190 ms (14.95%), Space: 58.9 MB (97.84%) - LeetHub

--- 11-container-with-most-water/11-container-with-most-water.cpp
+++ 11-container-with-most-water/11-container-with-most-water.cpp
@@ -5,11 +5,10 @@ public:
         int start=0,end=n-1,ans=0;
         while(end>start){
             ans = max(ans,(end-start)*min(v[start],v[end]));
-            if(v[start]<v[end]){
+            if(v[start]<v[end])
                 start++;
-            }else{
+            else
                 end--;
-            }
         }
         return ans;
     }
